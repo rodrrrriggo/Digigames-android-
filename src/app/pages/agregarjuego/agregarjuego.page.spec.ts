7dc1509d75270ea64d1b300a46c1ca8600aa65ae
rorro <rodguzm03@gmail.com>
2024-11-19 14:52:07 -0300

    primera prueba

--- src/app/pages/agregarjuego/agregarjuego.page.spec.ts
+++ src/app/pages/agregarjuego/agregarjuego.page.spec.ts
@@ -3,7 +3,6 @@ import { AgregarjuegoPage } from './agregarjuego.page';
 import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx';
 import { ServiceBDService } from 'src/app/services/service-bd.service';
 
-
 describe('AgregarjuegoPage', () => {
   let component: AgregarjuegoPage;
   let fixture: ComponentFixture<AgregarjuegoPage>;
@@ -13,15 +12,28 @@ describe('AgregarjuegoPage', () => {
       declarations: [AgregarjuegoPage],
       providers: [
         ServiceBDService,
-        { provide: SQLite}, // Usa el mock
+        { provide: SQLite }, 
       ],
     }).compileComponents();
 
-    const fixture = TestBed.createComponent(AgregarjuegoPage);
+    fixture = TestBed.createComponent(AgregarjuegoPage);
     component = fixture.componentInstance;
+
+    // Simula la función presentToast y ahora puede estar el espia en mi spec.ts :)
+    spyOn(component, 'presentToast').and.callFake(() => Promise.resolve());
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('El precio no puede ser negativo, de lo contrario no se puede agregar el juego', async () => {
+  
+    component.nombre = 'Call of Duty: Black Ops 6';
+    component.precio = -21400;
+    component.foto_producto = 'codbops6.jpg';
+    component.stock = 5;
+
+    
+    await component.crear();
+
+    
+    expect(component.presentToast).toHaveBeenCalledWith('El precio no puede ser negativo.');
   });
 });
